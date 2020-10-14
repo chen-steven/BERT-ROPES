d46f2b1f43941fbeb51b445fb6b758445d28d854
Steven Chen <syc.stevenchen@gmail.com>
2020-10-14 16:50:51 -0400

    modified utils

--- utils.py
+++ utils.py
@@ -9,3 +9,15 @@ def set_random_seed(seed):
     np.random.seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+
+def load_ropes_examples():
+    examples = []
+
+class ROPESExample:
+    def __init__(self, qas_id, question, context, answer, start_position):
+        self.qas_id = qas_id
+        self.question = question
+        self.context = context
+        self.answer = answer
+        self.start_character = start_position
+        self.end_position = start_position + len(answer)
