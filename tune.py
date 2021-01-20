4949fa9c1aad212175d677243e9aebf8be558de7
shiyue <byryuer@gmail.com>
2021-01-19 23:52:19 -0500

    run hotpot QA roberta baseline + bce

--- tune.py
+++ tune.py
@@ -10,7 +10,7 @@ def tune_baseline():
 def tune_baseline_bce():
     os.system("mkdir train/hotpot10_roberta_bce")
     os.system("python main_hp.py --batch-size 2 --epochs 3 --learning-rate 3e-5 --seed 10 "
-              "--output_dir train/hotpot10_roberta --gradient-accumulation-steps 4 --binary ")
+              "--output_dir train/hotpot10_roberta_bce --gradient-accumulation-steps 4 --binary ")
 
 
 if __name__ == '__main__':
